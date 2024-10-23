8c2fd387b071ed14f37e34d89b9c0b7acb278ca4
Imad <imad.abouu10@gmail.com>
2024-10-23 18:36:55 +0100

    fix

---
+++ atl3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+class Voiture {
+public:
+    string marque, modele;
+    int annee, kilometrage, vitesse ;
+
+    Voiture() : marque("any"), modele("any"), annee(2000), kilometrage(0), vitesse(100) {}
+
+
+    Voiture(const string& marque, const string& modele, int annee, int kilometrage, int vitesse) :
+        marque(marque), modele(modele), annee(annee), kilometrage(kilometrage), vitesse(vitesse) {}
+    
+    void acceler(float incresse){
+        vitesse += incresse ; 
+    }
+    void freiner(float decresse){
+        vitesse -= decresse ; 
+    }
+    void afficherInfo(){
+        cout << "Marque: " << marque << endl;
+        cout << "Modele: " << modele << endl;
+        cout << "Annee: " << annee << endl;
+        cout << "Kilometrage: " << kilometrage << endl;
+        cout << "Vitesse: " << vitesse << endl;
+    }
+    void avancer(float distance){
+        kilometrage += distance;
+    }
+    ~Voiture() { cout << "La voiture " << marque << " " << modele << " a été Destructe." << endl;}
+};
